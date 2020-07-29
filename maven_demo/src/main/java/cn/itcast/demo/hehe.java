cebc43c0a08b3e89c03ca6c173237f80f15b8ea9
si_mayi <17310675282@163.com>
2020-07-29 14:48:51 +0800

    ddd

--- maven_demo/src/main/java/cn/itcast/demo/hehe.java
+++ maven_demo/src/main/java/cn/itcast/demo/hehe.java
@@ -4,7 +4,8 @@ public class hehe {
     public static void main(String[] args) {
         System.out.println("aa");
         System.out.println("bb");
-        System.out.println("bb");
+        System.out.println("bbbb");
+        System.out.println("dd");
         System.out.println("zz");
 
     }
